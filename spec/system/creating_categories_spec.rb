a8f9c4cc886b172f678eaae49277111b4b8a8b5f
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-07-28 00:43:21 +0800

    Update test for creating categories

--- spec/system/creating_categories_spec.rb
+++ spec/system/creating_categories_spec.rb
@@ -5,5 +5,21 @@ RSpec.describe "CreatingCategories", type: :system do
     driven_by(:rack_test)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  it 'enables me to create categories' do
+    visit '/categories/new'
+
+    fill_in 'Title', with: 'Programming'
+    fill_in 'Details', with: 'Coding tasks for the day'
+
+    click_on 'Create Category'
+
+    expect(page).to have_content('Programming')
+    expect(page).to have_content('Coding tasks for the day')
+
+    category = Category.order('id').last
+    expect(category.title).to eq('Programming')
+    expect(category.description).to eq('Coding tasks for the day')
+  end
+
+  # pending "add some scenarios (or delete) #{__FILE__}"
 end
