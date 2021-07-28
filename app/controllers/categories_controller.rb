72868caa8769a1f02e9b8b3626ae54b975ce6a5c
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-07-28 19:06:31 +0800

    Update action for update category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,7 +15,15 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
   end
 
-  def update; end
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      redirect_to @category
+    else
+      render :edit
+    end
+  end
 
   private
 
