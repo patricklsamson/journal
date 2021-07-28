894e1f28af358b96d331f7260b747543e5154de4
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-07-28 13:44:05 +0800

    Update test for model existence

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'model existence test' do
+    it 'has none to begin with' do
+      expect(Category.count).to eq 0
+    end
+
+    it 'has one after adding one' do
+      Category.create
+      expect(Category.count).to eq 1
+    end
+  end
+
+  # pending "add some examples to (or delete) #{__FILE__}"
 end
