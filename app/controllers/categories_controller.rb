1eb437bfc1056385ebc8c4c8387be04b92d7b3e2
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-07-28 13:21:59 +0800

    Update create action from new to create

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    @category = Category.new(category_params)
+    @category = Category.create(category_params)
   end
 
   private
