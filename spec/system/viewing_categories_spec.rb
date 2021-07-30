39f22549587e93bc78de4fb9fc89035d09eb9517
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-07-30 23:29:35 +0800

    Update test for viewing categories

--- spec/system/viewing_categories_spec.rb
+++ spec/system/viewing_categories_spec.rb
@@ -5,5 +5,19 @@ RSpec.describe "ViewingCategories", type: :system do
     driven_by(:rack_test)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  it 'enables me to view a category' do
+    visit '/categories/new'
+
+    fill_in 'Title', with: 'Programming'
+    fill_in 'Details', with: 'Coding tasks for the day'
+
+    click_on 'Create Category'
+
+    visit root_path
+    click_link 'Programming'
+
+    expect(page).to have_content('Coding tasks for the day')
+  end
+
+  # pending "add some scenarios (or delete) #{__FILE__}"
 end
