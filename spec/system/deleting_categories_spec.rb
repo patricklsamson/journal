8d0277f60c3c11f550ef9b9b1f4133f846f16f88
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-07-31 22:24:55 +0800

    Update test for deleting categories

--- spec/system/deleting_categories_spec.rb
+++ spec/system/deleting_categories_spec.rb
@@ -1,9 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe "DeletingCategories", type: :system do
+RSpec.describe 'DeletingCategories', type: :system do
   before do
     driven_by(:rack_test)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  let(:click_new_category) { find('a[href="/categories/new"]').click }
+  let(:click_create_category) { find('input[type="submit"]').click }
+  let(:id) { Category.find_by(title: 'Category Title').id }
+  let(:click_destroy_category) { find("a[href='/categories/#{id}']").click }
+  let(:category_count) { Category.count }
+  let(:category) { Category.find_by(title: 'Category Title') }
+
+  it 'deletes a category' do
+    visit root_path
+    click_new_category
+
+    expect(page).to have_current_path(new_category_path)
+    fill_in 'Title', with: 'Category Title'
+    fill_in 'Details', with: 'Category Details'
+    click_create_category
+
+    expect(page).to have_current_path(category_path(id))
+    expect(page).to have_content('Category Title')
+    expect(page).to have_content('Category Details')
+
+    click_destroy_category
+    expect(category_count).to eq 0
+    expect(category).to eq nil
+  end
+
+  # pending "add some scenarios (or delete) #{__FILE__}"
 end
