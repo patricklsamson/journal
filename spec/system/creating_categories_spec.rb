edf78fcd758af33d7a186b62a5424f4ad9d3f8bb
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-07-31 22:28:45 +0800

    Update test for creating categories

--- spec/system/creating_categories_spec.rb
+++ spec/system/creating_categories_spec.rb
@@ -1,24 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe "CreatingCategories", type: :system do
+RSpec.describe 'CreatingCategories', type: :system do
   before do
     driven_by(:rack_test)
   end
 
-  it 'enables me to create categories' do
-    visit '/categories/new'
+  let(:click_new_category) { find('a[href="/categories/new"]').click }
+  let(:click_create_category) { find('input[type="submit"]').click }
+  let(:id) { Category.find_by(title: 'Category Title').id }
+  let(:category_count) { Category.count }
+  let(:category) { Category.find_by(title: 'Category Title') }
 
-    fill_in 'Title', with: 'Programming'
-    fill_in 'Details', with: 'Coding tasks for the day'
+  it 'creates a category' do
+    visit root_path
+    click_new_category
 
-    click_on 'Create Category'
+    expect(page).to have_current_path(new_category_path)
+    fill_in 'Title', with: 'Category Title'
+    fill_in 'Details', with: 'Category Details'
+    click_create_category
 
-    expect(page).to have_content('Programming')
-    expect(page).to have_content('Coding tasks for the day')
+    expect(page).to have_current_path(category_path(id))
+    expect(page).to have_content('Category Title')
+    expect(page).to have_content('Category Details')
 
-    category = Category.order('id').last
-    expect(category.title).to eq('Programming')
-    expect(category.details).to eq('Coding tasks for the day')
+    expect(category_count).to eq 1
+    expect(category.title).to eq('Category Title')
+    expect(category.details).to eq('Category Details')
   end
 
   # pending "add some scenarios (or delete) #{__FILE__}"
