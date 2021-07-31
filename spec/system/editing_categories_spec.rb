fd7878c788bc5f84563b3ffa3e31198f73a213ae
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-07-31 22:33:32 +0800

    Update test for editing categories

--- spec/system/editing_categories_spec.rb
+++ spec/system/editing_categories_spec.rb
@@ -1,50 +1,44 @@
 require 'rails_helper'
 
-RSpec.describe "EditingCategories", type: :system do
+RSpec.describe 'EditingCategories', type: :system do
   before do
     driven_by(:rack_test)
   end
 
-  it 'enables me to edit a title of category' do
-    visit '/categories/new'
+  let(:click_new_category) { find('a[href="/categories/new"]').click }
+  let(:click_create_category) { find('input[type="submit"]').click }
+  let(:id) { Category.find_by(title: 'Category Title').id }
+  let(:click_edit_category) { find("a[href='/categories/#{id}/edit']").click }
+  let(:click_update_category) { find('input[type="submit"]').click }
+  let(:category_count) { Category.count }
+  let(:category) { Category.find_by(title: 'Category Title Edited') }
 
-    fill_in 'Title', with: 'Programming'
-    fill_in 'Details', with: 'Coding tasks for the day'
+  it 'edits a category' do
+    visit root_path
+    click_new_category
 
-    click_on 'Create Category'
+    expect(page).to have_current_path(new_category_path)
+    fill_in 'Title', with: 'Category Title'
+    fill_in 'Details', with: 'Category Details'
+    click_create_category
 
-    id = Category.last.id.to_s
-    visit "/categories/#{id}"
-    click_link 'Edit'
+    expect(page).to have_current_path(category_path(id))
+    expect(page).to have_content('Category Title')
+    expect(page).to have_content('Category Details')
+    click_edit_category
 
-    expect(page).to have_content('Edit Category')
-    fill_in 'Title', with: 'Programming Edited'
-    click_on 'Update Category'
+    expect(page).to have_current_path(edit_category_path(id))
+    fill_in 'Title', with: 'Category Title Edited'
+    fill_in 'Details', with: 'Category Details Edited'
+    click_update_category
 
-    expect(page).to have_content('Programming Edited')
-    category = Category.find(id)
-    expect(category.title).to eq('Programming Edited')
-  end
+    expect(page).to have_current_path(category_path(id))
+    expect(page).to have_content('Category Title Edited')
+    expect(page).to have_content('Category Details Edited')
 
-  it 'enables me to edit details of category' do
-    visit '/categories/new'
-
-    fill_in 'Title', with: 'Programming'
-    fill_in 'Details', with: 'Coding tasks for the day'
-
-    click_on 'Create Category'
-
-    id = Category.last.id.to_s
-    visit "/categories/#{id}"
-    click_link 'Edit'
-
-    expect(page).to have_content('Edit Category')
-    fill_in 'Details', with: 'Coding tasks for the day edited'
-    click_on 'Update Category'
-
-    expect(page).to have_content('Coding tasks for the day edited')
-    category = Category.find(id)
-    expect(category.details).to eq('Coding tasks for the day edited')
+    expect(category_count).to eq 1
+    expect(category.title).to eq('Category Title Edited')
+    expect(category.details).to eq('Category Details Edited')
   end
 
   # pending "add some scenarios (or delete) #{__FILE__}"
