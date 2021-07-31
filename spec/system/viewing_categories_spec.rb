b7f8ad17c0e0568df836315ca889dc9476e97acb
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-07-31 22:33:51 +0800

    Update test for viewing categories

--- spec/system/viewing_categories_spec.rb
+++ spec/system/viewing_categories_spec.rb
@@ -1,22 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe "ViewingCategories", type: :system do
+RSpec.describe 'ViewingCategories', type: :system do
   before do
     driven_by(:rack_test)
   end
 
-  it 'enables me to view a category' do
-    visit '/categories/new'
+  let(:click_new_category) { find('a[href="/categories/new"]').click }
+  let(:click_create_category) { find('input[type="submit"]').click }
+  let(:id) { Category.find_by(title: 'Category Title').id }
+  let(:click_show_category) { find("a[href='/categories/#{id}']").click }
 
-    fill_in 'Title', with: 'Programming'
-    fill_in 'Details', with: 'Coding tasks for the day'
+  it 'views a category' do
+    visit root_path
+    click_new_category
 
-    click_on 'Create Category'
+    expect(page).to have_current_path(new_category_path)
+    fill_in 'Title', with: 'Category Title'
+    fill_in 'Details', with: 'Category Details'
+    click_create_category
+
+    expect(page).to have_current_path(category_path(id))
+    expect(page).to have_content('Category Title')
+    expect(page).to have_content('Category Details')
 
     visit root_path
-    click_link 'Programming'
+    click_show_category
 
-    expect(page).to have_content('Coding tasks for the day')
+    expect(page).to have_current_path(category_path(id))
+    expect(page).to have_content('Category Title')
+    expect(page).to have_content('Category Details')
   end
 
   # pending "add some scenarios (or delete) #{__FILE__}"
