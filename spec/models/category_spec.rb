07be4a52c1b23c8ca5c4edb7f06d17fd968163de
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-07-31 22:36:14 +0800

    Update test for category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,42 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  context 'model existence test' do
-    it 'has none to begin with' do
-      expect(Category.count).to eq 0
+  subject do
+    described_class.new(title: 'Category Title',
+                        details: 'Category Details')
+  end
+
+  let(:attributes) do
+    {
+      title: subject.title,
+      details: subject.details
+    }
+  end
+
+  let(:category_count) { Category.count }
+  let(:category_create) { Category.create(attributes) }
+
+  context 'when initialized' do
+    it 'counts to zero to begin with' do
+      expect(category_count).to eq 0
     end
 
-    it 'has one after adding one' do
-      Category.create(title: 'Programming',
-                      details: 'Coding tasks for the day')
-      expect(Category.count).to eq 1
+    it 'counts to one after adding one' do
+      category_create
+      expect(category_count).to eq 1
     end
   end
 
-  context 'validation test' do
-    subject do
-      described_class.new(title: 'Programming',
-                          details: 'Coding tasks for the day')
-    end
-
-    it 'is valid with valid attributes' do
+  context 'when all attributes are present' do
+    it 'does validate' do
       expect(subject).to be_valid
     end
+  end
 
-    it 'is not valid without a title' do
+  context 'without a title' do
+    it 'does not validate' do
       subject.title = nil
       expect(subject).to_not be_valid
     end
+  end
 
-    it 'is valid without details' do
+  context 'without details' do
+    it 'does not validate' do
       subject.details = nil
-      expect(subject).to be_valid
+      expect(subject).to_not be_valid
     end
+  end
 
-    it 'should have a unique title' do
-      Category.create(title: 'Programming')
-      another = Category.create(title: 'Programming')
+  context 'when title is not unique' do
+    it 'does not validate' do
+      category_create
+      another = Category.create(attributes)
       expect(another).to_not be_valid
+    end
+  end
+
+  context 'when details is less than 10 characters' do
+    it 'does not validate' do
+      subject.details = 'A' * 9
+      expect(subject).to_not be_valid
     end
   end
 
