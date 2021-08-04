9e75b2995cfc7e4f05cd2acf2d84f3f03a23ff0f
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-04 16:30:17 +0800

    Update test for viewing categories

--- spec/system/viewing_categories_spec.rb
+++ spec/system/viewing_categories_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'ViewingCategories', type: :system do
 
   let(:click_new_category) { find('a[href="/categories/new"]').click }
   let(:click_create_category) { find('input[type="submit"]').click }
-  let(:id) { Category.find_by(title: 'Category Title').id }
+  let(:category_id) { Category.find_by(title: 'Category Title').id }
   let(:click_show_category) { find("a[href='/categories/#{id}']").click }
 
   it 'views a category' do
@@ -19,14 +19,14 @@ RSpec.describe 'ViewingCategories', type: :system do
     fill_in 'Details', with: 'Category Details'
     click_create_category
 
-    expect(page).to have_current_path(category_path(id))
+    expect(page).to have_current_path(category_path(category_id))
     expect(page).to have_content('Category Title')
     expect(page).to have_content('Category Details')
 
     visit root_path
     click_show_category
 
-    expect(page).to have_current_path(category_path(id))
+    expect(page).to have_current_path(category_path(category_id))
     expect(page).to have_content('Category Title')
     expect(page).to have_content('Category Details')
   end
