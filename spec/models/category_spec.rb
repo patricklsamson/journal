1287089cbc01103a255a9b69990eb09ceb4942c2
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-05 01:08:10 +0800

    Update test for category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -26,13 +26,13 @@ RSpec.describe Category, type: :model do
     end
   end
 
-  context 'when all attributes are valid' do
+  context 'with valid attributes' do
     it 'does validate' do
       expect(subject).to be_valid
     end
   end
 
-  context 'when title is not present' do
+  context 'without a title' do
     it 'does not validate' do
       subject.title = nil || ''
       expect(subject).to_not be_valid
@@ -47,14 +47,14 @@ RSpec.describe Category, type: :model do
     end
   end
 
-  context 'when details is not present' do
+  context 'without details' do
     it 'does validate' do
       subject.details = nil
       expect(subject).to be_valid
     end
   end
 
-  context 'when associated' do
+  context 'with associations' do
     it 'has many tasks' do
       expect(category_on_tasks).to eq :has_many
     end
