c11bfff25e5c534f2a5a3a6c8ec64ca1e8403a98
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-04 17:37:07 +0800

    Update test for categories requests

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Categories', type: :request do
-  let(:attributes) do
+  let(:valid_attributes) do
     {
       title: 'Category Title',
       details: 'Category Details'
+    }
+  end
+
+  let(:invalid_attributes) do
+    {
+      title: nil,
+      details: nil
     }
   end
 
@@ -15,14 +22,12 @@ RSpec.describe 'Categories', type: :request do
     }
   end
 
-  subject do
-    Category.create(attributes)
-  end
-
+  subject { Category.create(valid_attributes) }
+  let(:category_invalid) { Category.create(invalid_attributes) }
   let(:category_count) { Category.count }
 
   describe 'GET /index' do
-    it 'finishes method successfully' do
+    it 'responds successfully' do
       get categories_path
       expect(response).to be_successful
     end
@@ -31,44 +36,76 @@ RSpec.describe 'Categories', type: :request do
   end
 
   describe 'GET /show' do
-    it 'finishes method successfully' do
+    it 'responds successfully' do
       get category_path(subject)
       expect(response).to be_successful
     end
   end
 
   describe 'GET /new' do
-    it 'finishes method successfully' do
+    it 'responds successfully' do
       get new_category_path
       expect(response).to be_successful
     end
   end
 
   describe 'GET /edit' do
-    it 'finishes method successfully' do
+    it 'responds successfully' do
       get edit_category_path(subject)
       expect(response).to be_successful
     end
   end
 
   describe 'POST /create' do
-    it 'finishes method successfully' do
-      post categories_path(subject), params: { category: attributes }
-      expect(category_count).to eq 1
-      expect(response).to be_successful
+    context 'when valid' do
+      before :each do
+        post categories_path(subject), params: { category: valid_attributes }
+      end
+
+      it 'creates a category' do
+        expect(category_count).to eq 1
+      end
+
+      it 'responds successfully' do
+        expect(response).to be_successful
+      end
+    end
+
+    context 'when invalid' do
+      before :each do
+        post categories_path(category_invalid), params: { category: invalid_attributes }
+      end
+
+      it 'does not create a category' do
+        expect(category_count).to eq 0
+      end
+
+      it 'responds successfully' do
+        expect(response).to be_successful
+      end
     end
   end
 
   describe 'PATCH /update' do
-    it 'finishes method successfully' do
-      patch category_path(subject), params: { category: new_attributes }
-      expect(category_count).to eq 1
-      expect(response).to redirect_to(:category)
+    context 'when valid' do
+      it 'redirects to itself' do
+        patch category_path(subject), params: { category: new_attributes }
+        expect(category_count).to eq 1
+        expect(response).to redirect_to(category_path(subject))
+      end
+    end
+
+    context 'when invalid' do
+      it 'responds successfully' do
+        patch category_path(subject), params: { category: invalid_attributes }
+        expect(category_count).to eq 1
+        expect(response).to be_successful
+      end
     end
   end
 
   describe 'DELETE /destroy' do
-    it 'finishes method successfully' do
+    it 'redirects to root path' do
       delete category_path(subject)
       expect(category_count).to eq 0
       expect(response).to redirect_to(root_path)
