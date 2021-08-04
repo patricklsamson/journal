de554da53d846ecddf354b72bbecb89c160b973e
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-05 01:08:49 +0800

    Update test for deleting categories

--- spec/system/deleting_categories_spec.rb
+++ spec/system/deleting_categories_spec.rb
@@ -7,23 +7,18 @@ RSpec.describe 'DeletingCategories', type: :system do
 
   let(:click_new_category) { find('a[href="/categories/new"]').click }
   let(:click_create_category) { find('input[type="submit"]').click }
-  let(:category) { Category.find_by(title: 'Category Title') }
-  let(:category_id) { category.id }
-  let(:click_destroy_category) { find("a[href='/categories/#{id}']").click }
+  let(:category_id) { Category.find_by(title: 'Category Title').id }
+  let(:click_destroy_category) { find("a[href='/categories/#{category_id}']").click }
   let(:category_count) { Category.count }
+  let(:category) { Category.find_by(title: 'Category Title') }
 
   it 'deletes a category' do
     visit root_path
     click_new_category
 
-    expect(page).to have_current_path(new_category_path)
     fill_in 'Title', with: 'Category Title'
     fill_in 'Details', with: 'Category Details'
     click_create_category
-
-    expect(page).to have_current_path(category_path(category_id))
-    expect(page).to have_content('Category Title')
-    expect(page).to have_content('Category Details')
 
     click_destroy_category
     expect(category_count).to eq 0
