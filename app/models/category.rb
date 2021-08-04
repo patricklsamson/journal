1ea08283d58b41d43719573ebf9de479036ed951
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-04 16:30:49 +0800

    Update category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   validates :title, presence: true,
                     uniqueness: true
-  validates :details, presence: true,
-                      length: { minimum: 10 }
+
+  has_many :tasks
 end
