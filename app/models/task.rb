8122c7b5a2542b9e00f06935dd5c111d29277894
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-04 16:37:03 +0800

    Add belongs to category

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,6 @@ class Task < ApplicationRecord
   validates :details, presence: true,
                       uniqueness: true,
                       length: { minimum: 10 }
+
+  belongs_to :category
 end
