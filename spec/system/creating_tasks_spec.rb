5c44be0c03b198aa8d239b4343bde537ed10f251
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-06 16:52:09 +0800

    Update test for creating tasks

--- spec/system/creating_tasks_spec.rb
+++ spec/system/creating_tasks_spec.rb
@@ -7,7 +7,16 @@ RSpec.describe 'CreatingTasks', type: :system do
 
   let(:click_new_category) { find('a[href="/categories/new"]').click }
   let(:click_create_category) { find('input[type="submit"]').click }
+  let(:click_create_task) { find('input[type="submit"]').click }
+
+  let(:date_today) { Date.today }
+
   let(:category_id) { Category.find_by(title: 'Category Title').id }
+
+  let(:task) { Task.find_by(details: 'Task Details') }
+  let(:task_details) { task.details }
+  let(:task_priority) { task.priority }
+  let(:task_count) { Task.count }
 
   before :each do
     visit root_path
@@ -18,30 +27,39 @@ RSpec.describe 'CreatingTasks', type: :system do
     click_create_category
   end
 
-  let(:click_create_task) { find('input[type="submit"]').click }
-  let(:task_count) { Task.count }
-  let(:task) { Task.find_by(details: 'Task Details') }
-  let(:task_details) { task.details }
-
-  context 'when valid' do
-    it 'creates a task' do
+  context 'when all form fields are filled up and submitted' do
+    before :each do
       fill_in 'Details', with: 'Task Details'
+      fill_in 'task[priority]', with: date_today
       click_create_task
+    end
 
+    it 'redirects to same page' do
       expect(page).to have_current_path(category_path(category_id))
-      expect(page).to have_content('Task Details')
+    end
 
-      expect(task_count).to eq 1
+    it 'renders page with submitted inputs' do
+      expect(page).to have_content('Task Details')
+      expect(page).to have_content(date_today)
+    end
+
+    it 'creates a task' do
       expect(task_details).to eq('Task Details')
+      expect(task_priority).to eq(date_today)
+      expect(task_count).to eq 1
     end
   end
 
-  context 'when invalid' do
-    it 'does not create a task' do
+  context 'when all form fields are not filled up and submitted' do
+    before :each do
       click_create_task
+    end
 
+    it 'redirects to same page' do
       expect(page).to have_current_path(category_path(category_id))
+    end
 
+    it 'does not create a task' do
       expect(task_count).to eq 0
       expect(task).to eq nil
     end
