11a08b0b5b79ae6341f809e17639fdd558f0729b
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-06 16:53:33 +0800

    Update test for viewing tasks

--- spec/system/viewing_tasks_spec.rb
+++ spec/system/viewing_tasks_spec.rb
@@ -8,10 +8,13 @@ RSpec.describe 'ViewingTasks', type: :system do
   let(:click_new_category) { find('a[href="/categories/new"]').click }
   let(:click_create_category) { find('input[type="submit"]').click }
   let(:click_create_task) { find('input[type="submit"]').click }
-  let(:category_id) { Category.find_by(title: 'Category Title').id }
   let(:click_show_category) { find("a[href='/categories/#{category_id}']").click }
 
-  it 'views a task' do
+  let(:date_today) { Date.today }
+
+  let(:category_id) { Category.find_by(title: 'Category Title').id }
+
+  before :each do
     visit root_path
     click_new_category
 
@@ -20,13 +23,20 @@ RSpec.describe 'ViewingTasks', type: :system do
     click_create_category
 
     fill_in 'Details', with: 'Task Details'
+    fill_in 'task[priority]', with: date_today
     click_create_task
 
     visit root_path
     click_show_category
+  end
 
+  it 'redirects to its category' do
     expect(page).to have_current_path(category_path(category_id))
+  end
+
+  it 'renders page with its contents' do
     expect(page).to have_content('Task Details')
+    expect(page).to have_content(date_today)
   end
 
   # pending "add some scenarios (or delete) #{__FILE__}"
