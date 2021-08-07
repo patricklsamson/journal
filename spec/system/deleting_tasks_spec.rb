5db7209c46e0792ddc7de8b63e711bc3eb6e81e7
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-08 00:49:16 +0800

    Update test for deleting tasks

--- spec/system/deleting_tasks_spec.rb
+++ spec/system/deleting_tasks_spec.rb
@@ -3,34 +3,38 @@ require 'rails_helper'
 RSpec.describe 'DeletingTasks', type: :system do
   before do
     driven_by(:rack_test)
+
+    category_create
+    task_create
+
+    visit category_path(category_id)
   end
 
-  let(:click_new_category) { find('a[href="/categories/new"]').click }
-  let(:click_create_category) { find('input[type="submit"]').click }
-  let(:click_create_task) { find('input[type="submit"]').click }
-  let(:click_destroy_task) { find("a[href='/categories/#{category_id}/tasks/#{task_id}']").click }
+  let(:date_today) { Date.today }
 
+  let(:category_create) { Category.create(title: 'Category Title', details: 'Category Details') }
   let(:category_id) { Category.find_by(title: 'Category Title').id }
 
+  let(:task_create) { Task.create(details: 'Task Details', priority: date_today, category_id: category_id) }
   let(:task_id) { Task.find_by(details: 'Task Details').id }
   let(:task_count) { Task.count }
+  let(:task_deleted) { Task.find_by(details: 'Task Details') }
 
-  let(:task) { Task.find_by(details: 'Task Details') }
+  let(:click_destroy_task) { find("a[href='/categories/#{category_id}/tasks/#{task_id}']").click }
 
-  it 'deletes a task' do
-    visit root_path
-    click_new_category
+  context 'when task was created within its category' do
+    it 'redirects to same page' do
+      expect(page).to have_current_path(category_path(category_id))
+    end
+  end
 
-    fill_in 'Title', with: 'Category Title'
-    fill_in 'Details', with: 'Category Details'
-    click_create_category
+  context 'with that task can be deleted' do
+    it 'deletes the task' do
+      click_destroy_task
 
-    fill_in 'Details', with: 'Task Details'
-    click_create_task
-
-    click_destroy_task
-    expect(task_count).to eq 0
-    expect(task).to eq nil
+      expect(task_count).to eq 0
+      expect(task_deleted).to eq nil
+    end
   end
 
   # pending "add some scenarios (or delete) #{__FILE__}"
