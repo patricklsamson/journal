3948d9501a0f4b2a10a0e3ac9c225f9c783a8be0
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-08 00:48:57 +0800

    Update test for creating tasks

--- spec/system/creating_tasks_spec.rb
+++ spec/system/creating_tasks_spec.rb
@@ -5,12 +5,10 @@ RSpec.describe 'CreatingTasks', type: :system do
     driven_by(:rack_test)
   end
 
-  let(:click_new_category) { find('a[href="/categories/new"]').click }
-  let(:click_create_category) { find('input[type="submit"]').click }
-  let(:click_create_task) { find('input[type="submit"]').click }
-
   let(:date_today) { Date.today }
+  let(:date_tomorrow) { date_today + 1 }
 
+  let(:category_create) { Category.create(title: 'Category Title', details: 'Category Details') }
   let(:category_id) { Category.find_by(title: 'Category Title').id }
 
   let(:task) { Task.find_by(details: 'Task Details') }
@@ -18,50 +16,89 @@ RSpec.describe 'CreatingTasks', type: :system do
   let(:task_priority) { task.priority }
   let(:task_count) { Task.count }
 
-  before :each do
-    visit root_path
-    click_new_category
+  let(:click_create_task) { find('input[type="submit"]').click }
 
-    fill_in 'Title', with: 'Category Title'
-    fill_in 'Details', with: 'Category Details'
-    click_create_category
+  before :each do
+    category_create
+    visit category_path(category_id)
   end
 
-  context 'when all form fields are filled up and submitted' do
+  context 'when a category was created it redirects to itself and a task can be created' do
+    it 'expects form inside' do
+      expect(page).to have_current_path(category_path(category_id))
+    end
+  end
+
+  context 'when all form fields were filled up and submitted' do
     before :each do
       fill_in 'Details', with: 'Task Details'
-      fill_in 'task[priority]', with: date_today
-      click_create_task
     end
 
-    it 'redirects to same page' do
-      expect(page).to have_current_path(category_path(category_id))
+    context 'with priority date was date today' do
+      before :each do
+        fill_in 'task[priority]', with: date_today
+        click_create_task
+      end
+
+      it 'creates a task' do
+        expect(task_details).to eq('Task Details')
+        expect(task_priority).to eq(date_today)
+        expect(task_count).to eq 1
+      end
+
+      it 'redirects to same page' do
+        expect(page).to have_current_path(category_path(category_id))
+      end
+
+      context 'when navigating under "Tasks for Today"' do
+        it 'shows created task' do
+          within('#today-wrap') { expect(page).to have_content('Task Details') }
+        end
+      end
     end
 
-    it 'renders page with submitted inputs' do
-      expect(page).to have_content('Task Details')
-      expect(page).to have_content(date_today)
-    end
+    context 'with priority date was date tomorrow' do
+      before :each do
+        fill_in 'task[priority]', with: date_tomorrow
+        click_create_task
+      end
 
-    it 'creates a task' do
-      expect(task_details).to eq('Task Details')
-      expect(task_priority).to eq(date_today)
-      expect(task_count).to eq 1
+      it 'creates a task' do
+        expect(task_details).to eq('Task Details')
+        expect(task_priority).to eq(date_tomorrow)
+        expect(task_count).to eq 1
+      end
+
+      it 'redirects to same page' do
+        expect(page).to have_current_path(category_path(category_id))
+      end
+
+      context 'when navigating under "Future Tasks"' do
+        it 'shows created task' do
+          within('#future-wrap') { expect(page).to have_content('Task Details') }
+        end
+      end
     end
   end
 
-  context 'when all form fields are not filled up and submitted' do
+  context 'when all form fields were not filled up and submitted' do
     before :each do
       click_create_task
-    end
-
-    it 'redirects to same page' do
-      expect(page).to have_current_path(category_path(category_id))
     end
 
     it 'does not create a task' do
       expect(task_count).to eq 0
       expect(task).to eq nil
+    end
+
+    it 'redirects to same page' do
+      expect(page).to have_current_path(category_path(category_id))
+    end
+
+    context 'with details blank' do
+      it 'renders page without changes' do
+        expect(page).to_not have_content('Task Details')
+      end
     end
   end
 
