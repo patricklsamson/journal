0709eca5c438db3c5b6ffe3c366b46fb52a4f4de
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-08 00:49:25 +0800

    Update test for editing categories

--- spec/system/editing_categories_spec.rb
+++ spec/system/editing_categories_spec.rb
@@ -5,56 +5,50 @@ RSpec.describe 'EditingCategories', type: :system do
     driven_by(:rack_test)
   end
 
-  let(:click_new_category) { find('a[href="/categories/new"]').click }
-  let(:click_create_category) { find('input[type="submit"]').click }
-  let(:click_update_category) { find('input[type="submit"]').click }
-  let(:click_edit_category) { find("a[href='/categories/#{category_id}/edit']").click }
-
+  let(:category_create) { Category.create(title: 'Category Title', details: 'Category Details') }
   let(:category) { Category.find_by(title: 'Category Title') }
   let(:category_id) { category.id }
   let(:category_title) { category.title }
   let(:category_details) { category.details }
-  let(:category_count) { Category.count }
 
   let(:category_edited) { Category.find_by(title: 'Category Title Edited') }
   let(:category_edited_title) { category_edited.title }
   let(:category_edited_details) { category_edited.details }
 
+  let(:click_edit_category) { find("a[href='/categories/#{category_id}/edit']").click }
+  let(:click_update_category) { find('input[type="submit"]').click }
+
   before :each do
-    visit root_path
-    click_new_category
-
-    fill_in 'Title', with: 'Category Title'
-    fill_in 'Details', with: 'Category Details'
-    click_create_category
-
+    category_create
+    visit category_path(category_id)
     click_edit_category
   end
 
-  it 'redirects to form' do
-    expect(page).to have_current_path(edit_category_path(category_id))
+  context 'when category was created it redirects to itself then it can be edited' do
+    it 'goes to form' do
+      expect(page).to have_current_path(edit_category_path(category_id))
+    end
   end
 
-  context 'when all form fields are filled up with new inputs and submitted' do
+  context 'when all form fields were filled up with new inputs and submitted' do
     before :each do
       fill_in 'Title', with: 'Category Title Edited'
       fill_in 'Details', with: 'Category Details Edited'
       click_update_category
     end
 
-    it 'redirects to edited category' do
+    it 'updates category' do
+      expect(category_edited_title).to eq('Category Title Edited')
+      expect(category_edited_details).to eq('Category Details Edited')
+    end
+
+    it 'redirects to updated category' do
       expect(page).to have_current_path(category_path(category_id))
     end
 
     it 'renders page with submitted inputs' do
       expect(page).to have_content('Category Title Edited')
       expect(page).to have_content('Category Details Edited')
-    end
-
-    it 'edits the category' do
-      expect(category_count).to eq 1
-      expect(category_edited_title).to eq('Category Title Edited')
-      expect(category_edited_details).to eq('Category Details Edited')
     end
   end
 
@@ -65,18 +59,19 @@ RSpec.describe 'EditingCategories', type: :system do
       click_update_category
     end
 
-    it 'redirects to same page' do
+    it 'does not update category' do
+      expect(category_title).to eq('Category Title')
+      expect(category_details).to eq('Category Details')
+    end
+
+    it 'redirects back to form' do
       expect(page).to have_current_path(category_path(category_id))
     end
 
-    it 'raises an error' do
-      expect(page).to have_content('blank')
-    end
-
-    it 'does not edit the category' do
-      expect(category_count).to eq 1
-      expect(category_title).to eq('Category Title')
-      expect(category_details).to eq('Category Details')
+    context 'with title blank' do
+      it 'raises an error' do
+        expect(page).to have_content('blank')
+      end
     end
   end
 
