07d72b6ced65b9afe7257f120a37bb6396c62740
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-08 00:49:55 +0800

    Update test for viewing tasks

--- spec/system/viewing_tasks_spec.rb
+++ spec/system/viewing_tasks_spec.rb
@@ -5,38 +5,43 @@ RSpec.describe 'ViewingTasks', type: :system do
     driven_by(:rack_test)
   end
 
-  let(:click_new_category) { find('a[href="/categories/new"]').click }
-  let(:click_create_category) { find('input[type="submit"]').click }
-  let(:click_create_task) { find('input[type="submit"]').click }
-  let(:click_show_category) { find("a[href='/categories/#{category_id}']").click }
-
   let(:date_today) { Date.today }
 
+  let(:category_create) { Category.create(title: 'Category Title', details: 'Category Details') }
   let(:category_id) { Category.find_by(title: 'Category Title').id }
 
+  let(:task_create) { Task.create(details: 'Task Details', priority: date_today, category_id: category_id) }
+
+  let(:click_show_category) { find("a[href='/categories/#{category_id}']").click }
+
   before :each do
-    visit root_path
-    click_new_category
-
-    fill_in 'Title', with: 'Category Title'
-    fill_in 'Details', with: 'Category Details'
-    click_create_category
-
-    fill_in 'Details', with: 'Task Details'
-    fill_in 'task[priority]', with: date_today
-    click_create_task
+    category_create
+    task_create
 
     visit root_path
     click_show_category
   end
 
-  it 'redirects to its category' do
+  it 'goes to its category' do
     expect(page).to have_current_path(category_path(category_id))
   end
 
-  it 'renders page with its contents' do
-    expect(page).to have_content('Task Details')
-    expect(page).to have_content(date_today)
+  context 'when navigating under "Overdue Tasks"' do
+    it 'shows "nothing" message' do
+      within('#overdue-wrap') { expect(page).to have_content('Nothing else here yet.') }
+    end
+  end
+
+  context 'when navigating under "Tasks for Today"' do
+    it 'shows created task' do
+      within('#today-wrap') { expect(page).to have_content('Task Details') }
+    end
+  end
+
+  context 'when navigating under "Future Tasks"' do
+    it 'shows "nothing" message' do
+      within('#future-wrap') { expect(page).to have_content('Nothing else here yet.') }
+    end
   end
 
   # pending "add some scenarios (or delete) #{__FILE__}"
