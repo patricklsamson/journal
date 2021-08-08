e2e36ab3417852cdfe55beb2b0fcf95d34fda3be
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-08 17:02:38 +0800

    Update test for deleting categories

--- spec/system/deleting_categories_spec.rb
+++ spec/system/deleting_categories_spec.rb
@@ -3,32 +3,39 @@ require 'rails_helper'
 RSpec.describe 'DeletingCategories', type: :system do
   before do
     driven_by(:rack_test)
-
-    category_create
-    visit category_path(category_id)
   end
 
-  let(:category_create) { Category.create(title: 'Category Title', details: 'Category Details') }
-  let(:category_id) { Category.find_by(title: 'Category Title').id }
-  let(:category_count) { Category.count }
-  let(:category_deleted) { Category.find_by(title: 'Category Title') }
-
-  let(:click_destroy_category) { find("a[href='/categories/#{category_id}']").click }
-
-  context 'when category was created' do
-    it 'redirects to itself' do
-      expect(page).to have_current_path(category_path(category_id))
+  describe Category do
+    let(:attributes) do
+      {
+        title: 'Category Title',
+        details: 'Category Details'
+      }
     end
-  end
 
-  context 'with that category can be deleted' do
+    subject { described_class.create(attributes) }
+    let(:click_destroy) { find("a[href='/categories/#{subject.id}']").click }
+
     before do
-      click_destroy_category
+      subject
+      visit category_path(subject)
     end
 
-    it 'deletes the category' do
-      expect(category_deleted).to eq nil
-      expect(category_count).to eq 0
+    context 'when category was created' do
+      it 'redirects to itself' do
+        expect(page).to have_current_path(category_path(subject))
+      end
+    end
+
+    context 'with that category can be deleted' do
+      before do
+        click_destroy
+      end
+
+      it 'deletes the category' do
+        expect(Category.find_by(attributes)).to eq nil
+        expect(Category.count).to eq 0
+      end
     end
   end
 
