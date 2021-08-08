babac6940148d4d59064201e0c232195224abc98
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-08 17:02:11 +0800

    Update test for tasks requests

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,137 +1,127 @@
 require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :request do
-  let(:date_today) { Date.today }
-  let(:date_tomorrow) { date_today + 1 }
-
-  let(:category_attributes) do
-    {
-      title: 'Category Title',
-      details: 'Category Details'
-    }
+  let(:category) do
+    Category.create(title: 'Category Title',
+                    details: 'Category Details')
   end
 
-  let(:category_create) { Category.create(category_attributes) }
-  let(:category_id) { Category.find_by(title: 'Category Title').id }
-
-  let(:valid_attributes) do
-    {
-      details: 'Task Details',
-      priority: date_today,
-      category_id: category_id
-    }
+  before do
+    category
   end
 
-  subject { Task.create(valid_attributes) }
-
-  let(:invalid_attributes) do
-    {
-      details: nil,
-      priority: nil,
-      category_id: nil
-    }
-  end
-
-  let(:new_attributes) do
-    {
-      details: 'Task Details Edited',
-      priority: date_tomorrow,
-      category_id: category_id
-    }
-  end
-
-  let(:task_invalid) { Task.create(invalid_attributes) }
-  let(:task_updated) { Task.find_by(new_attributes) }
-  let(:task_count) { Task.count }
-
-  before :each do
-    category_create
-  end
-
-  describe 'GET /edit' do
-    it 'responds sucessfully' do
-      get edit_category_task_path(category_id, subject)
-      expect(response).to be_successful
+  describe Task do
+    let(:valid_attributes) do
+      {
+        details: 'Task Details',
+        priority: Date.today,
+        category_id: category.id
+      }
     end
 
-    # pending "add some examples (or delete) #{__FILE__}"
-  end
-
-  describe 'POST /create' do
-    before :each do
-      post categories_path(category_id), params: { category: category_attributes }
+    let(:invalid_attributes) do
+      {
+        details: nil,
+        priority: nil,
+        category_id: nil
+      }
     end
 
-    context 'when valid' do
-      before :each do
-        post category_tasks_path(category_id, subject), params: { task: valid_attributes }
-      end
+    subject { described_class.create(valid_attributes) }
+    let(:subject_count) { Task.count }
 
-      it 'creates a task' do
-        expect(task_count).to eq 1
-      end
-
-      it 'redirects to its category' do
-        expect(response).to redirect_to(category_path(category_id))
-      end
-    end
-
-    context 'when invalid' do
-      before :each do
-        post category_tasks_path(category_id, task_invalid), params: { task: invalid_attributes }
-      end
-
-      it 'does not create a task' do
-        expect(task_count).to eq 0
-      end
-
-      it 'redirects to its category' do
-        expect(response).to redirect_to(category_path(category_id))
-      end
-    end
-  end
-
-  describe 'PATCH /update' do
-    context 'when valid' do
-      before do
-        patch category_task_path(category_id, subject), params: { task: new_attributes }
-      end
-
-      it 'updates task' do
-        expect(task_updated).to_not eq nil
-      end
-
-      it 'redirects to itself' do
-        expect(response).to redirect_to(category_path(category_id))
-      end
-    end
-
-    context 'when invalid' do
-      before do
-        patch category_task_path(category_id, subject), params: { task: invalid_attributes }
-      end
-
-      it 'does not update task' do
-        expect(task_updated).to eq nil
-      end
-
-      it 'responds successfully' do
+    describe 'GET /edit' do
+      it 'responds sucessfully' do
+        get edit_category_task_path(category, subject)
         expect(response).to be_successful
       end
-    end
-  end
 
-  describe 'DELETE /destroy' do
-    before :each do
-      delete category_task_path(category_id, subject)
+      # pending "add some examples (or delete) #{__FILE__}"
     end
 
-    it 'deletes the task' do
-      expect(task_count).to eq 0
+    describe 'POST /create' do
+      context 'when valid' do
+        before do
+          subject
+          post category_tasks_path(category), params: { task: valid_attributes }
+        end
+
+        it 'creates a task' do
+          expect(subject_count).to eq 1
+        end
+
+        it 'redirects to its category' do
+          expect(response).to redirect_to(category_path(category))
+        end
+      end
+
+      context 'when invalid' do
+        before do
+          post category_tasks_path(category), params: { task: invalid_attributes }
+        end
+
+        it 'does not create a task' do
+          expect(subject_count).to eq 0
+        end
+
+        it 'redirects to its category' do
+          expect(response).to redirect_to(category_path(category))
+        end
+      end
     end
 
-    it 'redirects to root path' do
-      expect(response).to redirect_to(category_path(category_id))
+    describe 'PATCH /update' do
+      let(:new_attributes) do
+        {
+          details: 'Task Details Edited',
+          priority: Date.tomorrow,
+          category_id: category.id
+        }
+      end
+
+      let(:subject_updated) { Task.find_by(new_attributes) }
+
+      context 'when valid' do
+        before do
+          patch category_task_path(category, subject), params: { task: new_attributes }
+        end
+
+        it 'updates task' do
+          expect(subject_updated).to_not eq nil
+        end
+
+        it 'redirects to itself' do
+          expect(response).to redirect_to(category_path(category))
+        end
+      end
+
+      context 'when invalid' do
+        before do
+          patch category_task_path(category, subject), params: { task: invalid_attributes }
+        end
+
+        it 'does not update task' do
+          expect(subject_updated).to eq nil
+        end
+
+        it 'responds successfully' do
+          expect(response).to be_successful
+        end
+      end
+    end
+
+    describe 'DELETE /destroy' do
+      before do
+        delete category_task_path(category, subject)
+      end
+
+      it 'deletes the task' do
+        expect(subject_count).to eq 0
+      end
+
+      it 'redirects to root path' do
+        expect(response).to redirect_to(category_path(category))
+      end
     end
   end
 end
