f3a71280ecbee00bbad99716c563852404b10871
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-08 17:02:01 +0800

    Update test for categories requests

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,142 +1,148 @@
 require 'rails_helper'
 
 RSpec.describe 'Categories', type: :request do
-  let(:valid_attributes) do
-    {
-      title: 'Category Title',
-      details: 'Category Details'
-    }
-  end
-
-  let(:invalid_attributes) do
-    {
-      title: nil,
-      details: nil
-    }
-  end
-
-  let(:new_attributes) do
-    {
-      title: 'Category Title Edited',
-      details: 'Category Details Edited'
-    }
-  end
-
-  subject { Category.create(valid_attributes) }
-  let(:category_invalid) { Category.create(invalid_attributes) }
-  let(:category_updated) { Category.find_by(new_attributes) }
-  let(:category_count) { Category.count }
-
-  describe 'GET /index' do
-    before do
-      get categories_path
+  describe Category do
+    let(:valid_attributes) do
+      {
+        title: 'Category Title',
+        details: 'Category Details'
+      }
     end
 
-    it 'responds successfully' do
-      expect(response).to be_successful
+    let(:invalid_attributes) do
+      {
+        title: nil,
+        details: nil
+      }
     end
 
-    # pending "add some examples (or delete) #{__FILE__}"
-  end
+    subject { described_class.create(valid_attributes) }
+    let(:subject_count) { Category.count }
 
-  describe 'GET /show' do
-    before do
-      get category_path(subject)
-    end
-
-    it 'responds successfully' do
-      expect(response).to be_successful
-    end
-  end
-
-  describe 'GET /new' do
-    before do
-      get new_category_path
-    end
-
-    it 'responds successfully' do
-      expect(response).to be_successful
-    end
-  end
-
-  describe 'GET /edit' do
-    before do
-      get edit_category_path(subject)
-    end
-
-    it 'responds successfully' do
-      expect(response).to be_successful
-    end
-  end
-
-  describe 'POST /create' do
-    context 'when valid' do
-      before :each do
-        post categories_path(subject), params: { category: valid_attributes }
-      end
-
-      it 'creates a category' do
-        expect(category_count).to eq 1
-      end
-
-      it 'responds successfully' do
-        expect(response).to be_successful
-      end
-    end
-
-    context 'when invalid' do
-      before :each do
-        post categories_path(category_invalid), params: { category: invalid_attributes }
-      end
-
-      it 'does not create a category' do
-        expect(category_count).to eq 0
-      end
-
-      it 'responds successfully' do
-        expect(response).to be_successful
-      end
-    end
-  end
-
-  describe 'PATCH /update' do
-    context 'when valid' do
+    describe 'GET /index' do
       before do
-        patch category_path(subject), params: { category: new_attributes }
+        get categories_path
       end
 
-      it 'updates category' do
-        expect(category_updated).to_not eq nil
+      it 'responds successfully' do
+        expect(response).to be_successful
       end
 
-      it 'redirects to itself' do
-        expect(response).to redirect_to(category_path(subject))
-      end
+      # pending "add some examples (or delete) #{__FILE__}"
     end
 
-    context 'when invalid' do
+    describe 'GET /show' do
       before do
-        patch category_path(subject), params: { category: invalid_attributes }
-      end
-
-      it 'does not update category' do
-        expect(category_updated).to eq nil
+        get category_path(subject)
       end
 
       it 'responds successfully' do
         expect(response).to be_successful
       end
     end
-  end
 
-  describe 'DELETE /destroy' do
-    before do
-      delete category_path(subject)
+    describe 'GET /new' do
+      before do
+        get new_category_path
+      end
+
+      it 'responds successfully' do
+        expect(response).to be_successful
+      end
     end
 
-    it 'redirects to root path' do
-      expect(category_count).to eq 0
-      expect(response).to redirect_to(root_path)
+    describe 'GET /edit' do
+      before do
+        get edit_category_path(subject)
+      end
+
+      it 'responds successfully' do
+        expect(response).to be_successful
+      end
+    end
+
+    describe 'POST /create' do
+      context 'when valid' do
+        before do
+          subject
+          post categories_path, params: { category: valid_attributes }
+        end
+
+        it 'creates a category' do
+          expect(subject_count).to eq 1
+        end
+
+        it 'responds successfully' do
+          expect(response).to be_successful
+        end
+      end
+
+      context 'when invalid' do
+        before do
+          post categories_path, params: { category: invalid_attributes }
+        end
+
+        it 'does not create a category' do
+          expect(subject_count).to eq 0
+        end
+
+        it 'responds successfully' do
+          expect(response).to be_successful
+        end
+      end
+    end
+
+    describe 'PATCH /update' do
+      let(:new_attributes) do
+        {
+          title: 'Category Title Edited',
+          details: 'Category Details Edited'
+        }
+      end
+
+      let(:subject_updated) { Category.find_by(new_attributes) }
+
+      context 'when valid' do
+        before do
+          patch category_path(subject), params: { category: new_attributes }
+        end
+
+        it 'updates category' do
+          expect(subject_updated).to_not eq nil
+        end
+
+        it 'redirects to itself' do
+          expect(response).to redirect_to(category_path(subject))
+        end
+      end
+
+      context 'when invalid' do
+        before do
+          patch category_path(subject), params: { category: invalid_attributes }
+        end
+
+        it 'does not update category' do
+          expect(subject_updated).to eq nil
+        end
+
+        it 'responds successfully' do
+          expect(response).to be_successful
+        end
+      end
+    end
+
+    describe 'DELETE /destroy' do
+      before do
+        delete category_path(subject)
+      end
+
+      it 'deletes the category' do
+        expect(subject_count).to eq 0
+      end
+
+      it 'redirects to root path' do
+        expect(response).to redirect_to(root_path)
+      end
     end
   end
 end
