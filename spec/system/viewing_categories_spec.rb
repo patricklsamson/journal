b2aecbf994b39d49372212c66c6ded3b2518f249
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-08 17:03:11 +0800

    Update test for viewing categories

--- spec/system/viewing_categories_spec.rb
+++ spec/system/viewing_categories_spec.rb
@@ -5,53 +5,55 @@ RSpec.describe 'ViewingCategories', type: :system do
     driven_by(:rack_test)
   end
 
-  let(:category_create) { Category.create(title: 'Category Title', details: 'Category Details') }
-  let(:category_id) { Category.find_by(title: 'Category Title').id }
-
-  let(:click_show_category) { find("a[href='/categories/#{category_id}']").click }
-
-  context 'when navigating in page of all categories' do
-    context 'when there are no categories yet' do
-      before do
-        visit categories_path
-      end
-
-      it 'shows heading' do
-        expect(page).to have_content('Categories')
-      end
-
-      it 'shows "create" message' do
-        expect(page).to have_content('Create a new category now!')
-      end
+  describe Category do
+    subject do
+      described_class.create(title: 'Category Title',
+                             details: 'Category Details')
     end
 
-    context 'when a category was created' do
-      before :each do
-        category_create
-        visit categories_path
-      end
-
-      it 'shows created category' do
-        expect(page).to have_content('Category Title')
-        expect(page).to have_content('Category Details')
-      end
-
-      it 'does not show "create" message' do
-        expect(page).to_not have_content('Create a new category now!')
-      end
-
-      context 'when viewing that category created' do
-        before :each do
-          click_show_category
+    context 'when navigating in page of all categories' do
+      context 'when there are no categories yet' do
+        before do
+          visit categories_path
         end
 
-        it 'goes to the category' do
-          expect(page).to have_current_path(category_path(category_id))
+        it 'shows heading' do
+          expect(page).to have_content('Categories')
         end
 
-        it 'renders page with its contents' do
+        it 'shows "create" message' do
+          expect(page).to have_content('Create a new category now!')
+        end
+      end
+
+      context 'when a category was created' do
+        before do
+          subject
+          visit categories_path
+        end
+
+        it 'shows created category' do
           expect(page).to have_content('Category Title')
           expect(page).to have_content('Category Details')
+        end
+
+        it 'does not show "create" message' do
+          expect(page).to_not have_content('Create a new category now!')
+        end
+
+        context 'when viewing that category created' do
+          before do
+            find("a[href='/categories/#{subject.id}']").click
+          end
+
+          it 'goes to the category' do
+            expect(page).to have_current_path(category_path(subject))
+          end
+
+          it 'renders page with its contents' do
+            expect(page).to have_content('Category Title')
+            expect(page).to have_content('Category Details')
+          end
         end
       end
     end
