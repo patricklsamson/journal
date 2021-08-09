b9f441208bd0d9166f82bf3073d4b14f2b3866b3
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-10 02:28:46 +0800

    Update test for category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,21 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  subject do
-    described_class.new(title: 'Category Title',
-                        details: 'Category Details')
+  let(:user) do
+    User.create(email: 'example@mail.com',
+                password: 'password')
   end
 
-  context 'when initialized' do
-    let(:subject_count) { Category.count }
+  subject do
+    described_class.new(title: 'Category Title',
+                        details: 'Category Details',
+                        user_id: user.id)
+  end
 
-    it 'counts to zero to begin with' do
-      expect(subject_count).to eq 0
+  before do
+    user
+  end
+
+  context 'with associations' do
+    it 'belongs to a user' do
+      expect(Category.reflect_on_association(:user).macro).to eq :belongs_to
     end
 
-    it 'counts to one after adding one' do
-      subject.save
-      expect(subject_count).to eq 1
+    it 'has many tasks' do
+      expect(Category.reflect_on_association(:tasks).macro).to eq :has_many
     end
   end
 
@@ -26,34 +33,36 @@ RSpec.describe Category, type: :model do
   end
 
   context 'without a title' do
+    before do
+      subject.title = nil
+    end
+
     it 'does not validate' do
-      subject.title = nil || ''
       expect(subject).to_not be_valid
     end
   end
 
   context 'when title is not unique' do
     before do
-      Category.create(title: 'Category Title',
-                      details: 'Category Details')
+      Category.create(title: subject.title,
+                      details: subject.details,
+                      user_id: subject.user_id)
+
+      subject.title = subject.title
     end
 
     it 'does not validate' do
-      subject.title = 'Category Title'
       expect(subject).to_not be_valid
     end
   end
 
   context 'without details' do
-    it 'does validate' do
+    before do
       subject.details = nil
-      expect(subject).to be_valid
     end
-  end
 
-  context 'with associations' do
-    it 'has many tasks' do
-      expect(Category.reflect_on_association(:tasks).macro).to eq :has_many
+    it 'does validate' do
+      expect(subject).to be_valid
     end
   end
 
