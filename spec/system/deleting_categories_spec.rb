de2105fd039bee6646a4270e608ef31f22bef347
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-10 02:30:17 +0800

    Update test for deleting categories

--- spec/system/deleting_categories_spec.rb
+++ spec/system/deleting_categories_spec.rb
@@ -1,42 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'DeletingCategories', type: :system do
-  before do
-    driven_by(:rack_test)
+  let(:user) do
+    User.create(email: 'example@mail.com',
+                password: 'password')
   end
 
-  describe Category do
-    let(:attributes) do
-      {
-        title: 'Category Title',
-        details: 'Category Details'
-      }
-    end
+  let(:attributes) do
+    {
+      title: 'Category Title',
+      details: 'Category Details',
+      user_id: user.id
+    }
+  end
 
-    subject { described_class.create(attributes) }
-    let(:click_destroy) { find("a[href='/categories/#{subject.id}']").click }
+  let(:category) do
+    Category.create(attributes)
+  end
 
-    before do
-      subject
-      visit category_path(subject)
-    end
+  before do
+    driven_by(:rack_test)
 
-    context 'when category was created' do
-      it 'redirects to itself' do
-        expect(page).to have_current_path(category_path(subject))
-      end
-    end
+    sign_in user
+    category
 
-    context 'with that category can be deleted' do
-      before do
-        click_destroy
-      end
+    visit category_path(category)
+    find("a[href='/categories/#{category.id}']").click
+  end
 
-      it 'deletes the category' do
-        expect(Category.find_by(attributes)).to eq nil
-        expect(Category.count).to eq 0
-      end
-    end
+  it 'deletes category' do
+    expect(Category.find_by(attributes)).to eq nil
   end
 
   # pending "add some scenarios (or delete) #{__FILE__}"
