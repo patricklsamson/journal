27a16f4041b5a8ece010768fe6fd941526f971aa
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-10 02:29:47 +0800

    Update test for creating categories

--- spec/system/creating_categories_spec.rb
+++ spec/system/creating_categories_spec.rb
@@ -1,71 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'CreatingCategories', type: :system do
-  before do
-    driven_by(:rack_test)
+  let(:user) do
+    User.create(email: 'example@mail.com',
+                password: 'password')
   end
 
-  describe Category do
-    subject { described_class.find_by(title: 'Category Title') }
-    let(:subject_count) { Category.count }
+  let(:category) do
+    Category.find_by(title: 'Category Title',
+                     details: 'Category Details')
+  end
 
-    let(:click_new) { find('a[href="/categories/new"]').click }
-    let(:click_create) { find('input[type="submit"]').click }
+  before do
+    driven_by(:rack_test)
 
-    before do
-      visit categories_path
-      click_new
-    end
+    sign_in user
+    visit categories_path
+    find('a[href="/categories/new"]').click
 
-    context 'when navigating in page of all categories then a category can be created' do
-      it 'goes to form' do
-        expect(page).to have_current_path(new_category_path)
-      end
-    end
+    fill_in 'Title', with: 'Category Title'
+    fill_in 'Details', with: 'Category Details'
+    find('input[type="submit"]').click
+  end
 
-    context 'when all form fields were filled up and submitted' do
-      before do
-        fill_in 'Title', with: 'Category Title'
-        fill_in 'Details', with: 'Category Details'
-        click_create
-      end
-
-      it 'creates a category' do
-        expect(subject.title).to eq('Category Title')
-        expect(subject.details).to eq('Category Details')
-        expect(subject_count).to eq 1
-      end
-
-      it 'redirects to created category' do
-        expect(page).to have_current_path(category_path(subject))
-      end
-
-      it 'renders page with submitted inputs' do
-        expect(page).to have_content('Category Title')
-        expect(page).to have_content('Category Details')
-      end
-    end
-
-    context 'when all form fields were not filled up and submitted' do
-      before do
-        click_create
-      end
-
-      it 'does not create a category' do
-        expect(subject).to eq nil
-        expect(subject_count).to eq 0
-      end
-
-      it 'redirects back to form' do
-        expect(page).to have_current_path(categories_path)
-      end
-
-      context 'with title blank' do
-        it 'raises an error' do
-          expect(page).to have_content('blank')
-        end
-      end
-    end
+  it 'creates category' do
+    expect(category).to_not eq nil
   end
 
   # pending "add some scenarios (or delete) #{__FILE__}"
