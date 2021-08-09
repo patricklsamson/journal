58a4764936eddd8cbc908d377cae9d90a1cf0af6
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-10 03:23:45 +0800

    Update test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'has many categories' do
-    expect(User.reflect_on_association(:categories).macro).to eq :has_many
+  subject do
+    described_class.create(email: 'example@mail.com',
+                           password: 'password')
   end
 
-  it 'has many tasks' do
-    expect(User.reflect_on_association(:tasks).macro).to eq :has_many
+  context 'with associations' do
+    it 'has many categories' do
+      expect(User.reflect_on_association(:categories).macro).to eq :has_many
+    end
+
+    it 'has many tasks' do
+      expect(User.reflect_on_association(:tasks).macro).to eq :has_many
+    end
+  end
+
+  context 'with dependents' do
+    let(:category) do
+      Category.create(title: 'Category Title',
+                      details: 'Category Details',
+                      user_id: subject.id)
+    end
+
+    let(:task) do
+      Task.create(details: 'Task Details',
+                  priority: Date.today,
+                  user_id: subject.id,
+                  category_id: category.id)
+    end
+
+    it 'deletes its categories' do
+      category
+      subject.destroy
+      expect(Category.count).to eq 0
+    end
+
+    it 'deletes its tasks' do
+      category
+      task
+      subject.destroy
+      expect(Task.count).to eq 0
+    end
   end
 
   # pending "add some examples to (or delete) #{__FILE__}"
