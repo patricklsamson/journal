434f1091925658a6f056a6e7fa880676e31cda66
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-10 02:30:28 +0800

    Update test for deleting tasks

--- spec/system/deleting_tasks_spec.rb
+++ spec/system/deleting_tasks_spec.rb
@@ -1,46 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe 'DeletingTasks', type: :system do
-  before do
-    driven_by(:rack_test)
-    category
+  let(:user) do
+    User.create(email: 'example@mail.com',
+                password: 'password')
   end
 
-  let(:category) { Category.create(title: 'Category Title', details: 'Category Details') }
+  let(:category) do
+    Category.create(title: 'Category Title',
+                    details: 'Category Details',
+                    user_id: user.id)
+  end
 
-  describe Task do
-    let(:attributes) do
-      {
-        details: 'Task Details',
-        priority: Date.today,
-        category_id: category.id
-      }
-    end
+  let(:attributes) do
+    {
+      details: 'Task Details',
+      priority: Date.today,
+      user_id: user.id,
+      category_id: category.id
+    }
+  end
 
-    subject { described_class.create(attributes) }
-    let(:click_destroy) { find("a[href='/categories/#{category.id}/tasks/#{subject.id}']").click }
+  let(:task) do
+    Task.create(attributes)
+  end
 
-    before do
-      subject
-      visit category_path(category)
-    end
+  before do
+    driven_by(:rack_test)
 
-    context 'when task was created within its category' do
-      it 'redirects to same page' do
-        expect(page).to have_current_path(category_path(category))
-      end
-    end
+    sign_in user
+    category
+    task
 
-    context 'with that task can be deleted' do
-      before do
-        click_destroy
-      end
+    visit category_path(category)
+    find("a[href='/categories/#{category.id}/tasks/#{task.id}']").click
+  end
 
-      it 'deletes the task' do
-        expect(Task.find_by(attributes)).to eq nil
-        expect(Task.count).to eq 0
-      end
-    end
+  it 'deletes the task' do
+    expect(Task.find_by(attributes)).to eq nil
   end
 
   # pending "add some scenarios (or delete) #{__FILE__}"
