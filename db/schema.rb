40f00257b5af7801763e57623f5145d2c08a9f23
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-09-30 19:06:42 +0800

    Add done attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_09_023824) do
+ActiveRecord::Schema.define(version: 2021_09_29_044213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_08_09_023824) do
     t.bigint "category_id", null: false
     t.datetime "priority"
     t.bigint "user_id", null: false
+    t.boolean "done", default: false
     t.index ["category_id"], name: "index_tasks_on_category_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
