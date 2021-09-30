45e872a5a43740d44fede8f62a6704c66bd445e6
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-09-30 19:06:51 +0800

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,77 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+Category.delete_all
+Task.delete_all
+
+user = User.create(
+  email: 'janedoe@mail.com',
+  password: 'janedoe'
+)
+
+user2 = User.create(
+  email: 'johnhamilton@mail.com',
+  password: 'johnhamilton'
+)
+
+category = Category.create(
+  title: 'Sample Category Title',
+  details: 'Sample Category Details',
+  user_id: user.id
+)
+
+category2 = Category.create(
+  title: 'Sample Category Title 2',
+  details: 'Sample Category Details 2',
+  user_id: user.id
+)
+
+category3 = Category.create(
+  title: 'Sample Category Title 3',
+  details: 'Sample Category Details 3',
+  user_id: user2.id
+)
+
+Task.create(
+  details: 'Sample Today Task',
+  priority: Date.current,
+  done: false,
+  user_id: user.id,
+  category_id: category.id
+)
+
+Task.create(
+  details: 'Sample Today Task 2',
+  priority: Date.current,
+  done: true,
+  user_id: user.id,
+  category_id: category.id
+)
+
+Task.create(
+  details: 'Sample Future Task',
+  priority: Date.current.tomorrow,
+  done: false,
+  user_id: user.id,
+  category_id: category2.id
+)
+
+Task.create(
+  details: 'Sample Task Done',
+  priority: Date.current,
+  done: true,
+  user_id: user2.id,
+  category_id: category3.id
+)
+
+task = Task.new(
+  details: 'Sample Past Task',
+  priority: Date.current.yesterday,
+  done: false,
+  user_id: user2.id,
+  category_id: category3.id
+)
+
+task.save(validate: false)
