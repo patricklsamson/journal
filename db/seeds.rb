aa15e89e4b4db12d57ae74338714ee426c1a6cc1
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-10-01 13:31:17 +0800

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Task.delete_all
-Category.delete_all
-User.delete_all
+User.destroy_all
 
 user = User.create(
   email: 'janedoe@mail.com',
